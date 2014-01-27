e4904f64bb33b6c231d8872ed660ea6961a13621
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-27 19:11:39 +0100

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
   def new
+    @user = User.new
   end
 
   def about
@@ -8,5 +15,22 @@ class UsersController < ApplicationController
 
   def contact
 
+  end
+
+  def create
+    @user = User.new(user_params)    
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      else
+        render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
